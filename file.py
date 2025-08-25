6ba0431d43e3b0167c9fd586a1c10117d9b9f193
smoke-story <gray0991@gmail.com>
2025-08-25 05:49:48 +0300

    project updated

--- file.py
+++
@@ -1,8 +0,0 @@
-from pages.doc import CiCd
-
-def test():
-
-	cicd = CiCd()
-	cicd.funk()
-	
-test()
--- pages/doc.py
+++
@@ -1,7 +0,0 @@
-import time
-
-class CiCd:
-
-	def funk(self):
-		for _ in range(50):
-			print("Hello, World!", time.strftime("%H:%M:%S"))
